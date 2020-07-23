f787bdce544c2df4a2b0b681bf32aaa5dacb85ea
Shashank <shashank95.a@gmail.com>
2020-07-23 17:03:54 +0530

    Added excel option to add viewers

--- src/view/shareBlockprobe/LimitSharedUsers.js
+++ src/view/shareBlockprobe/LimitSharedUsers.js
@@ -15,6 +15,8 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import Switch from '@material-ui/core/Switch';
 import TextField from '@material-ui/core/TextField';
+import Input from '@material-ui/core/Input';
+import * as XLSX from 'xlsx';
 import * as Utils from '../../common/utilSvc';
 import * as DbUtils from "../../common/dbSvc";
 import { isNullOrUndefined } from 'util';
@@ -51,13 +53,16 @@ const Transition = React.forwardRef(function Transition(props, ref) {
         this.renderUserList = this.renderUserList.bind(this);
         this.renderUser = this.renderUser.bind(this);
         this.renderUserDialog = this.renderUserDialog.bind(this);
+        this.renderAddViewersExcel = this.renderAddViewersExcel.bind(this);
         this.clickUser = this.clickUser.bind(this);
         this.removeViewer = this.removeViewer.bind(this);
         this.toggleDialog = this.toggleDialog.bind(this);
         this.changeUserLimitedState = this.changeUserLimitedState.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleChangeFile = this.handleChangeFile.bind(this);
         this.cancelUserCurrent = this.cancelUserCurrent.bind(this);
         this.addUserToViewList = this.addUserToViewList.bind(this);
+        this.addUsersToViewerList = this.addUsersToViewerList.bind(this);
     }
 
     toggleDialog(value, type){
@@ -322,10 +327,106 @@ const Transition = React.forwardRef(function Transition(props, ref) {
 
     }
 
-    cancelUserCurrent(){
+    cancelUserCurrent(){        
         this.setState({
             viewerId: ''
         })
+    }
+
+    async addUsersToViewerList(users){
+        let list = [{
+            id: this.props.uId,
+            role: "ADMIN"
+        }]; 
+        if(this.props.publicStatus.userList){
+            list = this.props.publicStatus.userList;
+        }
+        for(let i=0; users && i<users.length && list.length<LIMIT;i++){
+            let userId = users[i].trim();
+            let isUserPresent = false;
+            for(let j=0; j<list.length; j++){
+                if(list[j].id==userId)
+                {
+                    isUserPresent = true;
+                    break;
+                }
+            }
+
+            if(!isUserPresent && userId.length>0){
+                list.push({
+                    id: userId,
+                    role: "INVITED"
+                });
+            }
+        }
+        //console.log(list);
+        let publicStatus = JSON.parse(JSON.stringify(this.props.publicStatus));
+        publicStatus['userList'] = list;
+        this.setState({
+            addingUser: true
+        });
+        await firebase.firestore().collection("publicStatus").doc(this.props.bpId).set(publicStatus);    
+        this.setState({
+            addingUser: false
+        });
+    }
+
+    handleChangeFile(event){
+        let file = event.target.files[0];
+        let reader = new FileReader();
+        let scope = this;
+        reader.onload = function() {
+            const dataStr = reader.result;
+            const wb = XLSX.read(dataStr, {type:'binary'});
+            /* Get first worksheet */
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            /* Convert array of arrays */
+            const data = XLSX.utils.sheet_to_csv(ws, {header:1});
+            /* Update state */
+
+            let userIdData = [];
+            let dataArr = data.split('\n');
+            for(let i=0;dataArr && i<dataArr.length; i++){
+                let tempArr = dataArr[i].split(',');
+                if(tempArr.length > 0 && !isNullOrUndefined(tempArr[0]) &&
+                   tempArr[0].trim().length>0)
+                {
+                    userIdData.push(tempArr[0]);
+                }
+            }
+            scope.addUsersToViewerList(userIdData);
+            //scope.getFullTable(userIdData);
+          };
+        reader.readAsBinaryString(file);
+
+        //Done to remove earlier file and update with new file.
+        event.target.value = null;
+    }
+
+    renderAddViewersExcel(){
+        return (
+            <div style={{marginLeft:'10px', marginTop:'1em'}}>
+                <h4 style={{marginBottom: '10px'}}>Upload excel file with viewer userids</h4>
+                <h5 style={{marginTop: '0px'}}>User id must be the first column</h5>
+                <form>
+                    <label>
+                        <Input 
+                            type="file"
+                            variant="outlined"
+                            value={this.state.file}
+                            onChange={(e) => { this.handleChangeFile(e) }}
+                            rows="1"
+                            style={{
+                                background: 'white',
+                                marginTop:'6px',
+                                marginBottom:'6px',
+                                width:'30%'
+                                }}/>
+                    </label>
+                </form>                    
+            </div>
+        )
     }
 
     render(){
@@ -364,6 +465,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
                             null
                         }
                         {this.renderAddViewers()}
+                        {this.renderAddViewersExcel()}
                     </div>
                     :
                     null
